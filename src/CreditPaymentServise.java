755c4e7e0e33448592bc5f06f209b18b29b1e418
Михаил Синёв <msinev@masterdm.ru>
2023-05-20 01:13:28 +0300

    DONE!!!

---
+++ src/CreditPaymentServise.java
@@ -0,0 +1,14 @@
+public class CreditPaymentServise {
+
+    public double calculate(int s, int y){
+        double result;
+        double percent = 9.99;
+        double pd = percent / 100;
+        int mouth = y * 12;
+        double i = pd / 12 ;
+        double x = Math.pow((1 + i), mouth);
+        result = s * ((i * x) / (x -1));
+        return result;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+
+        CreditPaymentServise servise = new CreditPaymentServise();
+        double pay = servise.calculate(1_000_000, 1);
+        double pay1 = servise.calculate(1_000_000, 2);
+        double pay2 = servise.calculate(1_000_000, 3);
+
+        System.out.printf("Ежимесячный платёж на год = %.0f", pay);
+        System.out.println();
+        System.out.printf("Ежимесячный платёж на два года = %.0f", pay1);
+        System.out.println();
+        System.out.printf("Ежимесячный платёж на три года = %.0f", pay2);
+            }
+        }
+
