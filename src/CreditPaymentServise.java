c8db4317692e0a0dd1f7afedf095a5481b0bfb72
Михаил Синёв <msinev@masterdm.ru>
2023-05-26 01:00:56 +0300

    fix2

--- src/CreditPaymentServise.java
+++ src/CreditPaymentServise.java
@@ -1,13 +1,13 @@
 public class CreditPaymentServise {
 
-    public double calculate(int s, int y) {
+    public double calculate(int sum, int year) {
         double result;
         double percent = 9.99;
-        double pd = percent / 100;
-        int mouth = y * 12;
-        double i = pd / 12;
-        double x = Math.pow((1 + i), mouth);
-        result = s * ((i * x) / (x - 1));
+        double percentDecimal = percent / 100;
+        int mouth = year * 12;
+        double percentPerMonth = percentDecimal / 12;
+        //double x = Math.pow((1 + percentPerMonth), mouth);
+        result = sum * ((percentPerMonth * (Math.pow((1 + percentPerMonth), mouth))) / ((Math.pow((1 + percentPerMonth), mouth)) - 1));
         return result;
     }
 
